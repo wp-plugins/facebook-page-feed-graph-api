c3ec125df5db4d61b78c201081cf58bd1347c622
cameronjonesweb <cameronjonesweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-25 00:26:30 +0000

    Trunk
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-feed-graph-api/trunk@1145297 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebook-page-feed-graph-api.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Plugin Name: Facebook Page Feed (Graph API)
+ * Plugin URI: https://cameronjones.x10.mx/projects/facebook-page-plugin
+ * Description: Display the Facebook Page Plugin from the Graph API. 
+ * Version: 1.0
+ * Author: Cameron Jones
+ * Author URI: http://cameronjones.x10.mx
+ * License: GPLv2
+ 
+ * Copyright 2015  Cameron Jones  (email : cameronjonesweb@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+function facebook_page_plugin($filter) {
+	$return = NULL;
+	$a = shortcode_atts( array(
+        'href' => NULL,
+        'width' => NULL,
+		'height' => NULL,
+		'cover' => NULL,
+		'facepile' => NULL,
+		'posts' => NULL
+    ), $filter );
+	if(isset($a['href']) && !empty($a['href'])){
+		$return .= '<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=191521884244670&version=v2.3";fjs.parentNode.insertBefore(js, fjs);	}(document, \'script\', \'facebook-jssdk\'));</script>';
+		$return .= '<div class="fb-page" data-href="http://facebook.com/' . $a["href"] . '" ';
+		if(isset($a['width']) && !empty($a['width'])){
+			$return .= ' data-width="' . $a['width'] . '"';
+		}
+		if(isset($a['height']) && !empty($a['height'])){
+			$return .= ' data-width="' . $a['height'] . '"';
+		}
+		if(isset($a['cover']) && !empty($a['cover'])){
+			switch($a['cover']){
+				case "true":
+				$a['cover'] = false;
+				break;
+				case "false":
+				$a['cover'] = true;
+				break;
+			}
+			$return .= ' data-hide-cover="' . $a['cover'] . '"';
+		}
+		if(isset($a['facepile']) && !empty($a['facepile'])){
+			$return .= ' data-show-facepile="' . $a['facepile'] . '"';
+		}
+		if(isset($a['posts']) && !empty($a['posts'])){
+			$return .= ' data-show-posts="true"';
+		}
+		$return .= '></div>';
+	}
+	return $return;
+}
+add_shortcode( 'facebook-page-plugin', 'facebook_page_plugin' );
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Facebook Page Plugin ===
+Contributors: cameronjonesweb
+Tags: facebook,social,like,facepile,activity feed,recommendations,shortcode
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WLV5HPHSPM2BG&lc=AU&item_name=Cameron%20Jones%20Web%20Development¤cy_code=AUD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Requires at least: 4.0
+Tested up to: 4.1.2
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+Display the Facebook Page Plugin from the Graph API.
+
+== Description ==
+Facebook are depreciating many of their old social plugins on June 23rd 2015, including Activity Feed, Facepile, Like Box and Recommendations. As such, many WordPress plugins that utilise these social plugins will soon stop working. This plugin instead uses the Graph API v2.3 to guarantee your WordPress site continues to have full Facebook support.
+
+This plugin is shortcode based, unlike many other Facebook social plugins that are available. Simply call the [facebook-page-plugin] shortcode and set your settings within the shortcode. Place it wherever you like, as often as you like.
+
+== Installation ==
+1. Upload the entire `facebook-page-feed-graph-api` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+Usage: [facebook-page-plugin setting=\"value\"]. 
+Available settings: 
+href (URL path that comes after facebook.com/)
+width (number, in pixels, default 340)
+height (number, in pixels, default 500)
+cover (true/false, show page cover photo, default true)
+facepile (true/false, show facepile, default true)
+posts (true/false, display page posts, default false)
+Example: [facebook-page-plugin href=\"facebook\" width=\"300\" height=\"500\" cover=\"true\" facepile=\"true\" posts=\"true\"]
+
+== Frequently Asked Questions ==
+= What languages are available? =
+English.
+
+= My Facebook page isn\'t loading =
+If the URL for your page is http://facebook.com/ABC123 then when you use the shortcode don\'t include the domain, instead use like so: [facebook-page-plugin href=\"ABC123\"]
+Also, if your page has only just been created it may take some time for the API to load the page. Just be patient
+
+== Screenshots ==
+1. Installation example
+
+== Changelog ==
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+= 1.0 =
+Initial release
